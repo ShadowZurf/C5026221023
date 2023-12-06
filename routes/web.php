e5050e7bf6e1261f993565d6070431b86b12252f
Khalid Wildan Lazuardi <khalidlazuardi52@gmail.com>
2023-12-06 20:17:08 +0700

    Tugas pertemuan tanggal 30 Nop

--- routes/web.php
+++ routes/web.php
@@ -94,3 +94,6 @@ Route::get('/pegawai/hapus/{id}','App\Http\Controllers\PegawaiController@hapus')
 
 
 Route::get('/pegawai/cari','App\Http\Controllers\PegawaiController@cari');
+
+
+Route::get('/pegawai/view/{id}','App\Http\Controllers\PegawaiController@view');
